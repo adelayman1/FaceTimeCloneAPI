547df580caaa4d5a8dc7bbba219fc51b3f883a62
Adel Ayman <adelayman0000@gmail.com>
2023-02-03 01:08:37 +0200

    Update UserExtensions and add more functions

--- src/main/kotlin/com/adel/data/utilities/extensions/UserExtensions.kt
+++ src/main/kotlin/com/adel/data/utilities/extensions/UserExtensions.kt
@@ -3,6 +3,7 @@ package com.adel.data.utilities.extensions
 import com.adel.data.models.User
 import com.adel.domain.models.ParticipantModel
 import com.adel.domain.models.UserModel
+import java.util.regex.Pattern
 
 fun User.toUserModel() = UserModel(
     userName = name,
@@ -17,3 +18,19 @@ fun UserModel.toParticipantModel(missedCall:Boolean) = ParticipantModel(
     userId = userID,
     missedCall = missedCall
 )
+fun String.isEmailValid():Boolean{
+    return !isBlank() && length > 4 && Pattern.compile(
+        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+"
+    ).matcher(this).matches()
+}
+fun String.isPasswordValid():Boolean{
+    return !isBlank() && Pattern.compile(
+        "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}"
+    ).matcher(this).matches()
+}
